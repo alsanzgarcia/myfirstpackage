3ef8a3a5a5c39e3bfdbd2c9771a2edb8fc684a7b
Alejandro Sanz Garcia <asanz1234@gmail.com>
2025-01-03 15:07:58 +0100

    Update load_mat.R

--- R/load_mat.R
+++ R/load_mat.R
@@ -3,7 +3,7 @@
 #' This function loads a file as a matrix. It assumes that the first column
 #' contains the rownames and the subsequent columns are the sample identifiers.
 #' Any rows with duplicated row names will be dropped with the first one being
-#' kepted.
+#' kepted. Hello hello
 #'
 #' @param infile Path to the input file. Give full path
 #' @return A matrix of the infile. Nothing else
